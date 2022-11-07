1493607821227c2bb4da788ed4f55959ca058c6d
Edvard Astapovich <edvardastapovich@MacBook-Air-Edvard.local>
2022-11-07 20:20:45 +0400

    added form validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    // Slider
+
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider_arrow_left_solid.svg"></button>',
@@ -31,4 +33,57 @@ $(document).ready(function(){
     
     toggleSlide ('.catalog-item__link');
     toggleSlide ('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $(".overlay, #consultation").fadeIn()
+    })
+
+    $('.modal__close').on('click', function () {
+        $(".overlay, #consultation, #thanks, #order").fadeOut('slow')
+    })
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+            $(".overlay, #order").fadeIn()
+        })
+    })
+
+    // Form validate
+
+    function validForms (form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+    
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Минимум {0} символа должно быть!")
+                },
+                email: {
+                  required: "Пожалуйста, введите свой email",
+                  email: "Неправильный адрес почты"
+                },
+                phone: "Пожалуйста, введите свой телефон"
+              }
+        });
+    }
+
+    validForms ('#order form');
+    validForms ('#consultation-form');
+    validForms ('#consultation form');
+
+    $("input[name=phone]").mask("+375 (99) 999-99-99")
 });
